1be0cd6ca6b2002b2b0e9b8ff35df39fe6b2d4c0
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-07-16 09:50:51 +0700

    feat: configure structure app

---
+++ lib/core/core.dart
@@ -0,0 +1,4 @@
+export 'preferences/preferences.dart';
+export 'data/data.dart';
+export 'helpers/helpers.dart';
+export 'extensions/extensions.dart';
---
+++ lib/core/preferences/dimens.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Dimens {
+  Dimens._();
+
+  static width(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  static height(BuildContext context) {
+    return MediaQuery.of(context).size.height;
+  }
+
+  static const defaultSize = 16.0;
+
+  static const dp2 = 2.0;
+  static const dp4 = 4.0;
+  static const dp6 = 6.0;
+  static const dp8 = 8.0;
+  static const dp10 = 10.0;
+  static const dp12 = 12.0;
+  static const dp14 = 14.0;
+  static const dp16 = 16.0;
+  static const dp18 = 18.0;
+  static const dp20 = 20.0;
+  static const dp22 = 22.0;
+  static const dp24 = 24.0;
+  static const dp32 = 32.0;
+  static const dp40 = 40.0;
+  static const dp42 = 42.0;
+  static const dp48 = 48.0;
+  static const dp50 = 50.0;
+  static const dp200 = 200.0;
+}
