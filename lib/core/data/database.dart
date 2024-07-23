5d17c98b699258ba003b737148ed04c1b4728950
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-07-23 12:15:46 +0700

    feat: add authentication method login, register, getUser and logout

--- lib/core/data/database.dart
+++ lib/core/data/database.dart
@@ -1,9 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:news/features/profile/models/models.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Database {
-  Database({this.auth});
+  Database();
 
-  final String? auth;
+  String? auth;
 
   Dio get dio {
     return Dio()
@@ -25,7 +28,16 @@ class Database {
       if (response.statusCode == 200) {
         // Parsing response data
         final data = response.data;
-        return data['token'];
+        final token = data['token'];
+
+        // Save personal access token to SharedPreferences
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString('token', token);
+
+        // Set token dio Authorization
+        auth = token;
+
+        return data['message'];
       } else {
         // Handle different status codes and errors
         final data = response.data;
@@ -39,6 +51,89 @@ class Database {
       } else {
         // Some other error occurred
         return 'Error: ${e.message}';
+      }
+    }
+  }
+
+  Future<String> register({
+    required String username,
+    required String firstname,
+    String? lastname,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await dio.post(
+        'http://portal-berita.test/api/register',
+        data: {
+          "email": email,
+          "username": username,
+          "firstname": firstname,
+          "lastname": lastname,
+          "password": password,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return response.data['message'];
+      } else {
+        return response.data['message'];
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        // The server responded with an error
+        final data = e.response?.data;
+        throw ErrorDescription('${data['message'] ?? 'Unknown error'}');
+      } else {
+        // Some other error occurred
+        throw 'Error: ${e.message}';
+      }
+    }
+  }
+
+  Future<UserModel?> getUser() async {
+    try {
+      // Get token from SharedPreferences
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      auth = prefs.getString('token');
+
+      if (auth != null) {
+        final response = await dio.get('http://portal-berita.test/api/user');
+
+        if (response.statusCode == 200) {
+          final userData = response.data;
+          if (userData != null) {
+            return UserModel.fromJson(userData);
+          } else {
+            return null;
+          }
+        } else {
+          throw Exception('Failed to get user data');
+        }
+      } else {
+        throw Exception('No auth token found');
+      }
+    } on DioException catch (e) {
+      throw Exception('Network error: ${e.response?.data['message']}');
+    }
+  }
+
+  Future<void> logout() async {
+    try {
+      // Get token from SharedPreferences
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      auth = prefs.getString('token');
+
+      // Logout and remove personal access token from database
+      await dio.get('http://portal-berita.test/api/logout');
+
+      // remove token from SharedPreferences
+      prefs.remove('token');
+    } on DioException catch (e) {
+      if (e.response?.data['message'] is String) {
+        throw Exception('Network error: ${e.response?.data['message']}');
+      } else {
+        throw Exception('Unknown error occurred');
       }
     }
   }
