6564f8c73156b4cc2ff8b4c6cb4b87ba7dae0b8e
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-07-24 14:43:01 +0700

    feat: add page control

--- lib/features/control/pages/index/page.dart
+++ lib/features/control/pages/index/page.dart
@@ -1,13 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news/core/core.dart';
 
-class ControlPage extends StatelessWidget {
+part 'section/build_card.dart';
+
+class ControlPage extends StatefulWidget {
   const ControlPage({super.key});
 
   @override
+  State<ControlPage> createState() => _ControlPageState();
+}
+
+class _ControlPageState extends State<ControlPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Control your content'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.newspaper_rounded,
+                      size: 25,
+                      color: primary,
+                    ),
+                    Text(
+                      'NewsApp',
+                      style: GoogleFonts.comfortaa(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: primary),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: primary)),
+                hintText: 'Search your article',
+                prefixIcon: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Icon(Icons.search),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            Dimens.dp16.height,
+            Expanded(
+              child: ListView.separated(
+                itemBuilder: (context, index) {
+                  return const _BuildNewsCard(
+                    title: 'Mumbai local train',
+                    imageUrl: 'assets/images/train.png',
+                    description:
+                        'Local train services, considered as the lifeline o',
+                  );
+                },
+                itemCount: 2,
+                separatorBuilder: (BuildContext context, int index) {
+                  return Dimens.dp10.height;
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/features/control/pages/index/section/build_card.dart
@@ -0,0 +1,83 @@
+part of '../page.dart';
+
+class _BuildNewsCard extends StatelessWidget {
+  const _BuildNewsCard({
+    required this.title,
+    required this.imageUrl,
+    required this.description,
+  });
+
+  final String title;
+  final String imageUrl;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Row(
+          children: [
+            Container(
+              width: 120,
+              height: 120,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                image: DecorationImage(
+                    image: AssetImage(imageUrl), fit: BoxFit.cover),
+              ),
+            ),
+            const SizedBox(width: 15),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 4.0),
+                  Text(
+                    description,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 8.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: primary,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Dimens.dp10.width,
+                      CircleAvatar(
+                        backgroundColor: Colors.red.withOpacity(.8),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.delete_forever_rounded,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
