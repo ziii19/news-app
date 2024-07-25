5ce4af0c6d65cc2cb6adbc3588c23c2500bf1fb3
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-07-25 10:02:01 +0700

    feat: add bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app/app.dart';
+import 'core/core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Bloc.observer = AppBlockObserver();
   runApp(const MainApp());
 }
