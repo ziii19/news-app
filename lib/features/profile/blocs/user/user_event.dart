941bab888b2afaa4e734641427e0515b66b75f75
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-07-25 09:37:24 +0700

    feat: initial bloc user event

--- lib/features/profile/blocs/user/user_event.dart
+++ lib/features/profile/blocs/user/user_event.dart
@@ -3,4 +3,21 @@ part of 'user_bloc.dart';
 @freezed
 class UserEvent with _$UserEvent {
   const factory UserEvent.started() = _Started;
+
+  const factory UserEvent.getUser() = _GetUser;
+
+  const factory UserEvent.login({
+    required String email,
+    required String password,
+  }) = _LoginEvent;
+
+  const factory UserEvent.createUser({
+    required String username,
+    required String firstname,
+    required String lastname,
+    required String email,
+    required String password,
+  }) = _CreateUser;
+
+  const factory UserEvent.logout() = _LogoutEvent;
 }
