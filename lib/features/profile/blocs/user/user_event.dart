e8b573a23cce3a1743a5e8a1736dc9e62820a3ee
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-07-25 11:44:12 +0700

    feat: initial bloc user event

--- lib/features/profile/blocs/user/user_event.dart
+++ lib/features/profile/blocs/user/user_event.dart
@@ -1,8 +1,43 @@
 part of 'user_bloc.dart';
 
-sealed class UserEvent extends Equatable {
+class UserEvent extends Equatable {
   const UserEvent();
 
   @override
   List<Object> get props => [];
 }
+
+class GetUser extends UserEvent {}
+
+class RegisterEvent extends UserEvent {
+  final String username;
+  final String firstname;
+  final String lastname;
+  final String email;
+  final String password;
+
+  const RegisterEvent(
+      {required this.username,
+      required this.firstname,
+      required this.lastname,
+      required this.email,
+      required this.password});
+
+  @override
+  List<Object> get props => [username, firstname, lastname, email, password];
+}
+
+class LoginEvent extends UserEvent {
+  final String email;
+  final String password;
+
+  const LoginEvent({
+    required this.email,
+    required this.password,
+  });
+
+  @override
+  List<Object> get props => [email, password];
+}
+
+class LogoutEvent extends UserEvent {}
