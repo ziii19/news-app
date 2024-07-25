f70fc29dd4d82cdddaddaaf5d1d8d63b60d0d1cf
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-07-25 11:40:09 +0700

    feat: initial bloc user state

--- lib/features/profile/blocs/user/user_bloc.dart
+++ lib/features/profile/blocs/user/user_bloc.dart
@@ -1,11 +1,14 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news/features/profile/models/models.dart';
+
+import '../../../../core/core.dart';
 
 part 'user_event.dart';
 part 'user_state.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
-  UserBloc() : super(UserInitial()) {
+  UserBloc() : super(UserState.initial()) {
     on<UserEvent>((event, emit) {
       // TODO: implement event handler
     });
--- lib/features/profile/blocs/user/user_state.dart
+++ lib/features/profile/blocs/user/user_state.dart
@@ -1,10 +1,36 @@
 part of 'user_bloc.dart';
 
-sealed class UserState extends Equatable {
-  const UserState();
-  
-  @override
-  List<Object> get props => [];
-}
+class UserState extends Equatable {
+  const UserState({
+    this.status = Status.initial,
+    this.message,
+    this.error,
+    this.user,
+  });
 
-final class UserInitial extends UserState {}
+  final Status status;
+  final String? message;
+  final String? error;
+  final UserModel? user;
+
+  factory UserState.initial() {
+    return const UserState();
+  }
+
+  UserState copywith({
+    Status? status,
+    String? message,
+    String? error,
+    UserModel? user,
+  }) {
+    return UserState(
+      status: status ?? this.status,
+      message: message ?? message,
+      error: error ?? error,
+      user: user ?? user,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, message, error, user];
+}
