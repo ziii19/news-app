a498d6c3e90c4521114071faf12981e63aec7eba
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-07-25 09:52:00 +0700

    feat initial bloc user bloc

--- lib/features/profile/blocs/user/user_bloc.dart
+++ lib/features/profile/blocs/user/user_bloc.dart
@@ -11,7 +11,51 @@ part 'user_bloc.freezed.dart';
 class UserBloc extends Bloc<UserEvent, UserState> {
   UserBloc() : super(UserState.initial()) {
     on<UserEvent>((event, emit) {
-      // TODO: implement event handler
+      event.map(
+        started: (value) {
+          emit(UserState.initial());
+        },
+        getUser: (value) async {
+          try {
+            emit(state.copyWith(status: Status.loading));
+
+            final user = await Database().getUser();
+
+            emit(state.copyWith(status: Status.authenticated, user: user));
+          } catch (e) {
+            emit(state.copyWith(
+                status: Status.unauthenticated, error: e.toString()));
+          }
+        },
+        login: (value) async {
+          try {
+            emit(state.copyWith(status: Status.loading));
+
+            final response =
+                await Database().login(value.email, value.password);
+
+            if (response.$1) {
+              final user = await Database().getUser();
+
+              emit(state.copyWith(
+                  status: Status.success, message: response.$2, user: user));
+            } else {
+              emit(state.copyWith(status: Status.failure, error: response.$2));
+            }
+          } on Database catch (e) {
+            emit(state.copyWith(status: Status.failure, error: e.toString()));
+          }
+        },
+        createUser: (value) {},
+        logout: (value) async {
+          try {
+            await Database().logout();
+            emit(UserState.initial());
+          } catch (e) {
+            emit(state.copyWith(status: Status.failure, error: e.toString()));
+          }
+        },
+      );
     });
   }
 }
